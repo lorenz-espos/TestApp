4c625cac66e4c8d56c6f27eaeb12f403b0eec126
Lorenzo Esposito <72261684+lorenz-espos@users.noreply.github.com>
2022-04-11 16:29:50 +0200

    Add files via upload

--- lib/utils/user_secure_storage.dart
+++ lib/utils/user_secure_storage.dart
@@ -1,18 +1,22 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+//classe che mi permette di crittografare i tre campi acquisiti tramite i widgets
+//nella home
 class UserSecureStorage {
   static final _storage = FlutterSecureStorage();
-
+  //dichiaro le tre key impiegate per la crittografia
   static const _keyID = 'ID';
   static const _keyEmail = 'email';
   static const _keyBirthday = 'birthday';
-
+//ogni funzione di set impiega il dato ottenuto dall'utente tramite la key identificandolo
+//come value mentre la key viene usata per l operazione di crittografia
   static Future setID(String username) async =>
       await _storage.write(key: _keyID, value: username);
 
   static Future setEmail(String surname) async =>
       await _storage.write(key: _keyEmail, value: surname);
-
+//ogni funzione di get pesca il dato inserito nel database crittografato ed usa
+//la key per decrittografare l'informazione e mostarla a schermo all'utente
   static Future<String?> getID() async => await _storage.read(key: _keyID);
 
   static Future<String?> getEmail() async =>
